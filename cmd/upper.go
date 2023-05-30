524ce9e71441473e5c6ebfb72a56504033b94228
ohzqq <iamchurkey@gmail.com>
2023-05-30 14:04:52 -0400

    no date

--- cmd/upper.go
+++ cmd/upper.go
@@ -21,14 +21,4 @@ var upperCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(upperCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// upperCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// upperCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
