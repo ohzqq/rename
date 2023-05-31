ce9be684ccd6dc1abcff0c084e29d774d9b4b8b2
ohzqq <iamchurkey@gmail.com>
2023-05-31 10:54:13 -0400

    preview works

--- ui/preview.go
+++ ui/preview.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/charmbracelet/bubbles/viewport"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 	"github.com/londek/reactea"
 	"golang.org/x/term"
 )
@@ -14,16 +15,31 @@ type Preview struct {
 	reactea.BasicComponent
 	reactea.BasicPropfulComponent[PreviewProps]
 
-	view viewport.Model
+	view    viewport.Model
+	width   int
+	height  int
+	oldpath lipgloss.Style
+	newpath lipgloss.Style
 }
 
 type PreviewProps struct {
 	Names []map[string]string
 }
 
+var (
+	arrow   = lipgloss.NewStyle().Foreground(lipgloss.Color("#FF5F5F"))
+	confirm = lipgloss.NewStyle().Background(lipgloss.Color("#FF5F5F")).Foreground(lipgloss.Color("#262626"))
+)
+
 func NewPreview() *Preview {
+	w, h := TermSize()
+	d := lipgloss.NewStyle().Width(w)
 	return &Preview{
-		view: viewport.New(TermSize()),
+		width:   w,
+		height:  h,
+		oldpath: d,
+		newpath: d.Copy().Foreground(lipgloss.Color("#eeeeee")),
+		view:    viewport.New(w, h-1),
 	}
 }
 
@@ -31,12 +47,17 @@ func (c *Preview) Init(props PreviewProps) tea.Cmd {
 	c.UpdateProps(props)
 	var names []string
 	for _, name := range c.Props().Names {
+		var s strings.Builder
 		for og, n := range name {
-			names = append(names, og+" => "+n)
+			if og != n {
+				s.WriteString(c.oldpath.Render(og))
+				s.WriteString(arrow.Render("\n=> "))
+				s.WriteString(c.newpath.Render(n))
+				names = append(names, s.String())
+			}
 		}
 	}
-	c.view.SetContent(strings.Join(names, "\n"))
-
+	c.view.SetContent(strings.Join(names, "\n\n"))
 	return nil
 }
 
@@ -46,7 +67,17 @@ func (c *Preview) Update(msg tea.Msg) tea.Cmd {
 
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
-		if msg.String() == "ctrl+c" {
+		switch key := msg.String(); key {
+		case "y", "enter":
+			for _, name := range c.Props().Names {
+				for og, n := range name {
+					os.Rename(og, n)
+				}
+			}
+			fallthrough
+		case "n":
+			fallthrough
+		case "ctrl+c", "q":
 			return reactea.Destroy
 		}
 	}
@@ -57,7 +88,7 @@ func (c *Preview) Update(msg tea.Msg) tea.Cmd {
 }
 
 func (c *Preview) Render(w, h int) string {
-	return c.view.View()
+	return c.view.View() + "\n" + confirm.Render("rename? (y|enter/no)")
 }
 
 func TermSize() (int, int) {
