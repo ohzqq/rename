df786fdb1544a1131b1a3519e120fd5004dcd6c7
ohzqq <iamchurkey@gmail.com>
2023-05-31 14:34:29 -0400

    menu

--- ui/case.go
+++ ui/case.go
@@ -5,6 +5,7 @@ import "github.com/ohzqq/rename/cfg"
 func CaseForm() *Form {
 	inputs := make([]*Input, 1)
 	inputs[0] = NewInput(cfg.SetCase)
+	inputs[0].SetValue("3")
 	inputs[0].Prompt = casePrompt
 	return &Form{
 		inputs:  inputs,
@@ -15,7 +16,7 @@ func CaseForm() *Form {
 const casePrompt = `[0] CamelCase
 [1] kebab-case
 [2] lowerCamel
-[3] snake_case
+[3] snake_case (default)
 [4] lower
 [5] UPPER
 > `
